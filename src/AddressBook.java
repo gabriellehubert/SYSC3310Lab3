822987fc2f2f7b7fb815ee148c68211ae4eb7e3a
gabriellehubert <gabriellehubert@cb5109-17.labs.sce.carleton.ca>
2017-09-25 09:06:35 -0400

    I added some function calls to test GIT commit

---
+++ src/AddressBook.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+
+public class AddressBook 
+{
+	private ArrayList<BuddyInfo> book;
+	private int size;
+
+	public AddressBook() {
+		book = new ArrayList<BuddyInfo>();
+		size = 0;
+	}
+	
+	public ArrayList<BuddyInfo> getBook() {
+		return book;
+	}
+
+	public void setBook(ArrayList<BuddyInfo> book) {
+		this.book = book;
+	}
+	
+	public void addBuddy(BuddyInfo buddy)
+	{
+		book.add(buddy);
+		size++;
+	}
+	
+	public void removeBuddy(BuddyInfo buddy)
+	{
+		if(!book.remove(buddy))
+		{
+			System.out.println("Buddy not found.");
+		}
+		else
+		{
+			size--;
+		}
+		
+	}
+	
+	public void removeBuddy(int index)
+	{
+		if(index > size - 1)
+		{
+			System.out.println("Index out of range.");		
+		}
+		else
+		{
+			book.remove(index);
+			size--;
+		}
+		
+	}
+}
